65ad25604db0b03c4e19c289cb7843a780d03fac
ARaut9 <ankitraut999@gmail.com>
2019-08-13 23:32:01 +0530

    remove morgan code

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(bodyParser.json())
-morgan.token('data', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+// morgan.token('data', (req, res) => JSON.stringify(req.body))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
 app.use(express.static('build'))
 
